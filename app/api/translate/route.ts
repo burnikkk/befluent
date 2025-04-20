eda7fe773be342bb787bccade373c1a79ad099a4
master <197489738+burnikkk@users.noreply.github.com>
2025-04-20 19:22:17 +0200

    Remove unused Recorder logic and improve code usability.
    Deleted obsolete `Recorder_init.tsx` and refactored `useSpeak` for direct functionality. Added `use client` directive to relevant components, switched to environment variables for API keys, and updated global styles for light mode. Included an `.env.example` template for API key setup.

--- app/api/translate/route.ts
+++ app/api/translate/route.ts
@@ -2,8 +2,7 @@ import { NextResponse } from 'next/server';
 import OpenAI from 'openai';
 
 const openai = new OpenAI({
-  apiKey:
-    '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',
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 export async function POST(request: Request) {
@@ -46,42 +45,3 @@ export async function POST(request: Request) {
     );
   }
 }
-
-// export default function handler(req: NextApiRequest, res: NextApiResponse) {
-//   if (req.method === 'POST') {
-//     res.status(200).json({ message: 'Hello from Next.js!' })
-//     // Process a POST request
-//   } else {
-//     // Handle any other HTTP method
-//   }
-// }
-
-// export async function POST(request: Request) {
-//   const { language, text } = await request.json();
-//
-//   const response = await openai.chat.completions.create({
-//     model: 'gpt-4o-mini',
-//     messages: [
-//       {
-//         'role': 'system',
-//         'content': `
-//           You will be provided with a sentence. Your tasks are to:
-//           - Detect what language the sentence is in
-//           - Translate the sentence into ${language}
-//           Do not return anything other than the translated sentence.
-//         `,
-//       },
-//       {
-//         'role': 'user',
-//         'content': text,
-//       },
-//     ],
-//     temperature: 0.7,
-//     max_tokens: 64,
-//     top_p: 1,
-//   });
-//
-//   return NextResponse.json({
-//     text: `${language} : ${text}`,
-//   });
-// }
--- app/hooks/useSpeak.ts
+++ app/hooks/useSpeak.ts
@@ -5,13 +5,11 @@ import { useAvailableVoices } from '@/app/hooks/useAvailableVoices';
 export const useSpeak = () => {
   const { activeVoice } = useAvailableVoices();
 
-  const speak = (text: string) => {
+  return (text: string) => {
     const utterance = new SpeechSynthesisUtterance(text);
     if (activeVoice) {
       utterance.voice = activeVoice;
     }
     window.speechSynthesis.speak(utterance);
   };
-
-  return speak;
 };
